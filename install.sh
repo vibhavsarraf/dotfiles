764e018146d5d214deb0e03416ba748d68de0934
vibhav.s <vibhav.s@gmail.com>
2019-07-03 20:47:39 +0530

    Binaries added

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "Creating symlinks"
-
 echo -e "Creating ~/.config Directory"
 echo "=============================="
 if [ ! -d "$HOME/.config" ]; then
@@ -11,12 +9,23 @@ if [ ! -d "$HOME/.config" ]; then
     mkdir -p "$HOME/.config"
 fi
 
+echo "Installing Oh-my-zsh"
+
 curl -Lo install1.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 sh install1.sh --unattended
 rm install1.sh
 rm ~/.zshrc
+
+echo "Creating symlinks"
+
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/nvim ~/.config/nvim
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+echo "Installing Binaries"
+
+bin_files=$( echo bin/* )
+for file in $bin_files; do
+  cp $file /usr/local/bin/
+done
