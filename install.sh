1c1d10fab1b1e92effe6da5853bc88f7d16c5e23
vibhavsarraf <vidgaf@gmail.com>
2019-07-03 11:59:00 +0530

    cp to ln -s

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #bin/bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp $PWD/zshrc ~/.zshrc
-cp $PWD/vimrc ~/.vimrc
+ln -s $PWD/zshrc ~/.zshrc
+ln -s $PWD/vimrc ~/.vimrc
