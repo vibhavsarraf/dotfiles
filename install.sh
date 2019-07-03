1c32d5b56e836b83dabe29cfcc8ecd6bc8cc4d79
vibhav.s <vibhav.s@gmail.com>
2019-07-03 18:11:51 +0530

    Updates

--- install.sh
+++ install.sh
@@ -4,8 +4,16 @@
 
 echo "Creating symlinks"
 
+echo -e "Creating ~/.config Directory"
+echo "=============================="
+if [ ! -d "$HOME/.config" ]; then
+    echo "Creating ~/.config"
+    mkdir -p "$HOME/.config"
+fi
+
 ln -s $PWD/oh-my-zsh ~/.oh-my-zsh
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/nvim ~/.config/nvim
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
